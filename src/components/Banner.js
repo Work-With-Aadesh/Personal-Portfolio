07969eb541f2000c96474f6f1467ca8c6b4a3923
AadeshLawange-Guardinger <aadesh.lawange@guardinger.in>
2024-06-22 15:39:15 +0530

    let's connect arrow update with functionality.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,6 +57,11 @@ export const Banner = () => {
     }
   };
 
+  const handleDownload = () => {
+    const downloadUrl = "https://drive.google.com/file/d/11KqmBu2r54pquliK3So1-CyIaJA8I_bk/view?usp=sharing";
+    window.open(downloadUrl, '_blank');
+  };
+
   return (
     <section className="banner" id="home">
       <Container>
@@ -89,13 +94,30 @@ export const Banner = () => {
                     and trends to drive continuous improvement and innovation.
                   </p>
                   <button
-                    onClick={() =>
-                      document
-                        .getElementById("connect")
-                        .scrollIntoView({ behavior: "smooth" })
-                    }
+                    onClick={handleDownload}
+                    style={{
+                      fontWeight: "400",
+                      fontSize: "15px",
+                      border: "1px solid #3b3d49",
+                      transition: "all .5s ease",
+                      borderRadius: "40px",
+                      padding: "0px 60px 0px 60px",
+                      height: "85px",
+                      lineHeight: "70px",
+                    }}
                   >
-                    Let’s Connect <ArrowRightCircle size={25} />
+                    Get Resume
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-download"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
+                      <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z" />
+                    </svg>
                   </button>
                 </div>
               )}
